505e4219b234ef93eb7bd429e9940fdd6622f44b
Luis Enrique Mendez Cantero <luis-enrique.mendez-cantero@telekom.de>
2024-11-04 19:51:41 +0100

    android: add comment about turbine

--- android_apps/LabTestCoroutinesFlows/app/src/main/java/com/oceloti/lemc/labtestcoroutinesflows/flows/FlowViewModel.kt
+++ android_apps/LabTestCoroutinesFlows/app/src/main/java/com/oceloti/lemc/labtestcoroutinesflows/flows/FlowViewModel.kt
@@ -33,7 +33,7 @@ class FlowViewModel(
     val password = _password.asStateFlow()
 
 
-    //TODO: Test this hot flow
+    //TODO: Test this hot flow with turbine
     val canRegister = email
         .debounce(500L)
         .combine(
